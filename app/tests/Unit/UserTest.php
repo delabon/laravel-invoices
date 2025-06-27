aca6e70d12e653c81bfda7100745bbbf59063fc3
Sabri Taieb <sabritaiebdev@gmail.com>
2025-06-27 19:39:10 +0100

    feat: `to array` unit test for the User model to make sure all properties/columns exists

---
+++ app/tests/Unit/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+
+test('to array', function () {
+    $user = User::factory()->create();
+
+    expect($user->toArray())->toHaveKeys([
+        'id',
+        'name',
+        'email',
+        'email_verified_at',
+        'created_at',
+        'updated_at'
+    ])
+        ->not
+        ->toHaveKey('passowrd')
+        ->not
+        ->toHaveKey('remember_token');
+});
