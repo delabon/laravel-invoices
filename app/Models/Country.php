7f1328b909c76b579e4e12617e00b794aa57f4cf
Sabri Taieb <sabritaiebdev@gmail.com>
2025-11-27 09:36:52 +0100

    refactor: tests

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -6,4 +6,7 @@ namespace App\Models;
 
 use Squire\Models\Country as SquireCountry;
 
-final class Country extends SquireCountry {}
+final class Country extends SquireCountry
+{
+    public const int CODE_LENGTH = 2;
+}
--- app/Models/Region.php
+++ app/Models/Region.php
@@ -6,4 +6,9 @@ namespace App\Models;
 
 use Squire\Models\Region as SquireRegion;
 
-final class Region extends SquireRegion {}
+final class Region extends SquireRegion
+{
+    public const int CODE_MIN_LENGTH = 3;
+
+    public const int CODE_MAX_LENGTH = 6;
+}
